999d984cebff1af2fcb770d4948b11e893685985
40234272 <gtaylor18@qub.ac.uk>
2021-11-26 16:31:25 +0000

    added Dockerfle and requirements. Added response headers

--- src/app.py
+++ src/app.py
@@ -6,35 +6,29 @@ from palindrome import palindrome_count
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     sentence = ''
     num_of_palindromes = 0
 
-    if request.args.get('text') == '':
-        output = {
-            "error": False,
-            "sentence entered": "",
-            "answer": 0
-        }    
-    else:
-        sentence = request.args.get('text')
-        num_of_palindromes = palindrome_count(sentence)
+    sentence = request.args.get('text')
+    num_of_palindromes = palindrome_count(sentence)
 
-        output = {
-            "error": False,
-            "sentence entered": sentence,
-            "answer": num_of_palindromes
-        }
-
-    print(f"Sentence: {sentence}, Palindrome Count: {num_of_palindromes}")
+    output = {
+        "error": False,
+        "sentence entered": sentence,
+        "answer": num_of_palindromes
+    }
 
     json_output = json.dumps(output)
     response = flask.Response(json_output)
+    response.headers['Content-Type'] = 'application/json'
+    response.headers['Access-Control-Allow-Origin'] = '*'
 
     return response
 
-
+# Error handler to deal with incorrect parameters 
 @app.errorhandler(500)
 def server_error_500(error):
     error_output = {
@@ -45,6 +39,8 @@ def server_error_500(error):
 
     json_output = json.dumps(error_output)
     response = flask.Response(json_output)
+    response.headers['Content-Type'] = 'application/json'
+    response.headers['Access-Control-Allow-Origin'] = '*'
 
     return response
 
