e87ddc5dbf5c10d1a69e6996f346f56704cf382e
40234272 <gtaylor18@qub.ac.uk>
2021-11-26 16:15:45 +0000

    created Flask server to deal with the palindrome count. Added error handling for 500 server errors

--- src/app.py
+++ src/app.py
@@ -1,21 +1,53 @@
-from flask import Flask, request
+from logging import error
+import flask
+import json
+from flask import Flask, request, Response
 from palindrome import palindrome_count
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    sentence = request.args.get('text')
-    num_of_palindromes = palindrome_count(sentence)
+    sentence = ''
+    num_of_palindromes = 0
 
-    output = {
-        "error": False,
-        "sentence entered": sentence,
-        "answer": num_of_palindromes
-    }
+    if request.args.get('text') == '':
+        output = {
+            "error": False,
+            "sentence entered": "",
+            "answer": 0
+        }    
+    else:
+        sentence = request.args.get('text')
+        num_of_palindromes = palindrome_count(sentence)
+
+        output = {
+            "error": False,
+            "sentence entered": sentence,
+            "answer": num_of_palindromes
+        }
 
     print(f"Sentence: {sentence}, Palindrome Count: {num_of_palindromes}")
-    return num_of_palindromes
+
+    json_output = json.dumps(output)
+    response = flask.Response(json_output)
+
+    return response
+
+
+@app.errorhandler(500)
+def server_error_500(error):
+    error_output = {
+        "error": False,
+        "sentence entered": "500 Error: Incorrect Parameters Used",
+        "answer": 0    
+    }
+
+    json_output = json.dumps(error_output)
+    response = flask.Response(json_output)
+
+    return response
+
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0')
--- src/palindrome.py
+++ src/palindrome.py
@@ -2,7 +2,9 @@ def palindrome_count(text_input):
 
     count = 0
     sentence_list = text_input.split()
-
+    
+    # iterate through sentence list and compare each word greater than one letter with its reverse version
+    # if its a match, increment the count
     for word in sentence_list:
         if word == word[::-1] and len(word) > 1:
             count += 1
