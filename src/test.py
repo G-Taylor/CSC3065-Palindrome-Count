02255b880e09ff9b3e5aace7c6cedd1b3125b26b
40234272 <gtaylor18@qub.ac.uk>
2021-12-02 16:44:30 +0000

    fixed typo in test names

--- src/test.py
+++ src/test.py
@@ -19,41 +19,41 @@ class PalindromeTest(unittest.TestCase):
 
 
     # Web API tests
-    def test_palidrome_count_1(self):
+    def test_palindrome_count_1(self):
         response = self.app.get('/?text=lol')
         assert response.data.decode("utf-8") == '{"error": false, "sentence": "lol", "answer": 1}'
 
 
-    def test_palidrome_count_10(self):
+    def test_palindrome_count_10(self):
         response = self.app.get('/?text=lol aha anna, kayak, not a palindrome, civic, radar, madam racecar rotor stats')
         assert response.data.decode("utf-8") == '{"error": false, "sentence": "lol aha anna, kayak, not a palindrome, civic, radar, madam racecar rotor stats", "answer": 10}'
 
 
-    def test_palidrome_count_remove_special_chars(self):
+    def test_palindrome_count_remove_special_chars(self):
         response = self.app.get('/?text=spec-ial char:s sh@ould <a>ll b{e gone a;ha')
         assert response.data.decode("utf-8") == '{"error": false, "sentence": "spec-ial char:s sh@ould <a>ll b{e gone a;ha", "answer": 1}'
 
 
-    def test_palidrome_count_remove_numbers(self):
+    def test_palindrome_count_remove_numbers(self):
         response = self.app.get('/?text=This str1ng should h4v3 thre3 palindromes m4ad4am t3enet w0ow')
         assert response.data.decode("utf-8") == '{"error": false, "sentence": "This str1ng should h4v3 thre3 palindromes m4ad4am t3enet w0ow", "answer": 3}'
 
 
-    def test_palidrome_count_empty_string(self):
+    def test_palindrome_count_empty_string(self):
         response = self.app.get('/?text=')
         assert response.data.decode("utf-8") == '{"error": false, "sentence": "", "answer": 0}'
 
 
-    def test_palidrome_count_incorrect_parameters_no_query(self):
+    def test_palindrome_count_incorrect_parameters_no_query(self):
         response = self.app.get('/')
         assert response.data.decode("utf-8") == '{"error": true, "sentence": "500 Error: Incorrect Parameters Used", "answer": 0}'
 
 
-    def test_palidrome_count_incorrect_parameters_no_equals_sign(self):
+    def test_palindrome_count_incorrect_parameters_no_equals_sign(self):
         response = self.app.get('/?text')
         assert response.data.decode("utf-8") == '{"error": false, "sentence": "", "answer": 0}'
 
-    def test_palidrome_count_incorrect_parameters_wrong(self):
+    def test_palindrome_count_incorrect_parameters_wrong(self):
         response = self.app.get('/text=this is a test')
         assert response.data.decode("utf-8") == '{"error": true, "sentence": "404 Error: Page Not Found", "answer": 0}'
 
